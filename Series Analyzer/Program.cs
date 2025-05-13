83b35a8ecde432377e11ba5a7228dcee4054a8ae
asher zeev aush <A0583251203@gmail.com>
2025-05-13 17:39:37 +0300

    upted manu

--- Series Analyzer/Program.cs
+++ Series Analyzer/Program.cs
@@ -94,25 +94,95 @@ namespace Series_Analyzer
                 }
                 return ;
             }
-            CheckIfThereAreNumberInArgs();
-            foreach (int ser in series1) { Console.WriteLine(ser); }
+            //CheckIfThereAreNumberInArgs();
+            //foreach (int ser in series1) { Console.WriteLine(ser); }
             //foreach (int ser in series) { Console.WriteLine(ser); }
 
 
-                
+                // menu
+
+                void Menu()
+                {
+                    Console.WriteLine("Select a number from the menu (1-10)");
+                    Console.WriteLine("1. Input a Series.");
+                    Console.WriteLine("2. Display the series in the order it was entered.");
+                    Console.WriteLine("3. Display the series in the reversed order it was\r\nentered.");
+                    Console.WriteLine("4. Display the series in sorted order (from low to\r\nhigh).");
+                    Console.WriteLine("5. Display the Max value of the series.");
+                    Console.WriteLine("6. Display the Min value of the series.");
+                    Console.WriteLine("7. Display the Average of the series.");
+                    Console.WriteLine("8. Display the Number of elements in the series.");
+                    Console.WriteLine("9. Display the Sum of the series.");
+                    Console.WriteLine("10. Exit.");
+                    Console.WriteLine("Enter your choice: ");
+                    int choice = int.Parse(Console.ReadLine());
+
+                    switch (choice)
+                        {
+                        case 1:
+                            InputUserNumber();
+                            break;
+                        case 2:
+                            Console.WriteLine("The series you entered: ");
+                            foreach (int ser in series1)
+                            {
+                                Console.WriteLine(ser);
+                            }
+                            break;
+                        case 3:
+                            Console.WriteLine("The series you entered in reverse order: ");
+                            series1.Reverse();
+                            foreach (int ser in series1)
+                            {
+                                Console.WriteLine(ser);
+                            }
+                            break;
+                        case 4:
+                            Console.WriteLine("The series you entered in sorted order: ");
+                            series1.Sort();
+                            foreach (int ser in series1)
+                            {
+                                Console.WriteLine(ser);
+                            }
+                            break;
+                        case 5:
+                            Console.WriteLine("The max value of the series is: ");
+                            Console.WriteLine(series1.Max());
+                            break;
+                        case 6:
+                            Console.WriteLine("The min value of the series is: ");
+                            Console.WriteLine(series1.Min());
+                            break;
+
+                        case 7:
+                            Console.WriteLine("The average of the series is: ");
+                            Console.WriteLine(series1.Average());
+                            break;
+                        case 8:
+                            Console.WriteLine("The number of elements in the series is: ");
+                            Console.WriteLine(series1.Count());
+                            break;
+                        case 9:
+                            Console.WriteLine("The sum of the series is: ");
+                            Console.WriteLine(series1.Sum());
+                            break;
+                        case 10:
+                            Console.WriteLine("Exiting the program...");
+                            break;
+                        }
 
 
 
-
-
-
-
-
+                }
+                CheckIfThereAreNumberInArgs();
+                Menu();
 
 
 
 
 
         }
+
+        }
     }
-}
+
